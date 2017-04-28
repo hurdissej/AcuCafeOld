e63aa2b13e4080b29a767752cdb9886a62fcc86f
hurdissej <hurdissej@me.com>
2017-04-28 17:56:29 +0100

    update

--- ClientApp/js/controllers/drinkController.js
+++ ClientApp/js/controllers/drinkController.js
@@ -10,11 +10,15 @@
         ]);
 
     function drinkController($scope, $http,$routeParams){
-         $scope.drinks = [];
+         $scope.drinksCart = [];
+         $scope.optionsCart = [];
          $scope.options = [];
+         $scope.cart = [];
+         $scope.total = 0.00;
          $scope.loadingDrinks = true;
          $scope.loadingOptions = true;
-         // To do - Code own CORS //
+
+        // To do - Code own CORS //
          $http.get('https://cors-anywhere.herokuapp.com/http://acucafe.acumen.rocks/api/Drink/')
              .then(function(response){
                 $scope.drinks = response.data;
@@ -28,7 +32,24 @@
                 })
              .finally(function(){
                  $scope.loadingOptions = false;
-             })
-         ;
-     }
+             });
+        //
+        $scope.addDrinksToCart = function(acuOrder, option){
+            $scope.drinksCart.push(acuOrder);
+            //var value = Math.round((acuOrder.price * option)*100)/100;
+            $scope.total += Math.round((acuOrder.price * option)*100)/100;
+            console.log($scope.drinksCart);
+        };
+
+        $scope.addOptionsToCart = function(acuOrder, option){
+            $scope.optionsCart.push(acuOrder);
+            //var value = Math.round((acuOrder.price * option)*100)/100;
+            $scope.total += Math.round((acuOrder.price * option)*100)/100;
+            console.log($scope.optionsCart);
+        };
+
+        $scope.submitOrder = function(cart){
+            console.log(drinksCart);
+        }
+    }
 })();
