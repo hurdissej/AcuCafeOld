cdff1a4d4a36c9d1f85b2c21cd2251b044876f5f
hurdissej <hurdissej@me.com>
2017-05-04 20:24:29 +0100

    convert current to TS

--- ClientApp/js/controllers/drinkController.js
+++ ClientApp/js/controllers/drinkController.js
@@ -1,109 +1,119 @@
 /**
  * Created by elliot.hurdiss on 26/04/2017.
  */
-(function(){
+var app;
+(function (app) {
     'use strict';
-    angular
-        .module('acuCafe')
-        .controller('drinkController',[
-            '$scope', '$http', drinkController
-        ]);
-
-    function drinkController($scope, $http,$routeParams){
-        // For API
-         $scope.loadingDrinks = true;
-         $scope.loadingOptions = true;
-         $scope.drink = 0;
-         $scope.optionIDs = [];
-        // For UI
-         $scope.basketEmpty = true;
-         $scope.userDrink = '';
-         $scope.userOptions = [];
-         $scope.allOrders = [];
-         $scope.total = 0;
-         $scope.runningTotal = 0;
-
-        // To do - Code own CORS function or update server side //
-         $http.get('https://cors-anywhere.herokuapp.com/http://acucafe.acumen.rocks/api/Drink/')
-             .then(function(response){
-                $scope.drinks = response.data;
-                })
-             .finally(function(){
-                 $scope.loadingDrinks = false;
-             });
-         $http.get('https://cors-anywhere.herokuapp.com/http://acucafe.acumen.rocks/api/Option/')
-             .then(function(response){
-                $scope.options = response.data;
-                })
-             .finally(function(){
-                 $scope.loadingOptions = false;
-             });
-        //
-        $scope.addDrink = function (drinkID, description, price) {
-            price = Math.round(price *100) /100;
-            $scope.drink = drinkID;
-            $scope.userDrink = description;
-            $scope.runningTotal += price ;
-            console.log($scope.runningTotal);
+    var drinkController = (function () {
+        function drinkController($http) {
+            var _this = this;
+            this.$http = $http;
+            this.loadingDrinks = true;
+            this.loadingOptions = true;
+            this.drink = 0;
+            this.optionIDs = [];
+            this.basketEmpty = true;
+            this.userDrink = '';
+            this.userOptions = [];
+            this.allOrders = [];
+            this.total = 0;
+            this.runningTotal = 0;
+            this.drinks = [];
+            this.options = [];
+            this.getDrinks().then(function (result) {
+                _this.drinks = result;
+                _this.loadingDrinks = false;
+            });
+            this.getOptions().then(function (result) {
+                _this.options = result;
+                _this.loadingOptions = false;
+            });
+        }
+        drinkController.prototype.addDrink = function (drinkID, description, price) {
+            price = Math.round(price * 100) / 100;
+            this.drink = drinkID;
+            this.userDrink = description;
+            this.runningTotal += price;
+            console.log(this.runningTotal);
         };
-
-        $scope.optionSelection = function toggleSelection(optionID, optionDescription, optionPrice) {
-            var idx = $scope.optionIDs.indexOf(optionID);
-            var price = Math.round(optionPrice * 100)/100;
+        ;
+        drinkController.prototype.optionSelection = function (optionID, optionDescription, optionPrice) {
+            console.log(optionID);
+            var idx = this.optionIDs.indexOf(optionID);
+            var price = Math.round(optionPrice * 100) / 100;
             // Is currently selected
             if (idx > -1) {
-                $scope.optionIDs.splice(idx, 1);
-                $scope.userOptions.splice(idx, 1);
-                $scope.runningTotal -= price ;
+                this.optionIDs.splice(idx, 1);
+                this.userOptions.splice(idx, 1);
+                this.runningTotal -= price;
             }
-             // Is newly selected
             else {
-                $scope.optionIDs.push(optionID);
-                $scope.userOptions.push(optionDescription);
-                $scope.runningTotal += price ;
+                this.optionIDs.push(optionID);
+                this.userOptions.push(optionDescription);
+                this.runningTotal += price;
             }
         };
-
-        $scope.exit = function(){
-            $scope.runningTotal = 0;
+        ;
+        drinkController.prototype.exit = function () {
+            this.runningTotal = 0;
         };
-
-        $scope.orderDrink= function(optionIDs){
+        ;
+        drinkController.prototype.getDrinks = function () {
+            return this.$http({
+                method: 'get',
+                url: 'https://cors-anywhere.herokuapp.com/http://acucafe.acumen.rocks/api/Drink/'
+            }).then(function (result) {
+                return result.data;
+            });
+        };
+        drinkController.prototype.getOptions = function () {
+            return this.$http({
+                method: 'get',
+                url: 'https://cors-anywhere.herokuapp.com/http://acucafe.acumen.rocks/api/Option/'
+            }).then(function (result) {
+                return result.data;
+            });
+        };
+        drinkController.prototype.orderDrink = function (optionIDs) {
             // Create User friendly data version for display
             var userfriendlyDrinks = {
-                            "drink": $scope.userDrink,
-                            "options": $scope.userOptions
-                        };
-
+                "drink": this.userDrink,
+                "options": this.userOptions
+            };
             // Create API data
             var drinks = {
                 "drinks": [
-                        {
-                            "drinkId": $scope.drink,
-                            "optionIds": $scope.optionIDs
-                        }]};
-
-            $http.post('https://cors-anywhere.herokuapp.com/http://acucafe.acumen.rocks/api/Order', drinks)
-            .then(function(response) {
-                    // To do make some nice Bootstrap pop-ups //
-                    alert("Your order will be with you shortly!!");
-
-                    // Clear UI
-                    $scope.allOrders.push(userfriendlyDrinks);
-                    $scope.basketEmpty = false;
-                    $scope.userOptions = [];
-                    $scope.total += $scope.runningTotal;
-                    $scope.runningTotal = 0;
-                    // Uncheck checkboxes
-                    angular.forEach($scope.options, function(option) {
-                        option.Selected = false;
-                    })
-                },
-                function () {
-                    alert("Could not post your order");
-                });
-
+                    {
+                        "drinkId": this.drink,
+                        "optionIds": this.optionIDs
+                    }
+                ]
+            };
+            // Clear UI
+            this.allOrders.push(userfriendlyDrinks);
+            this.basketEmpty = false;
+            this.userOptions = [];
+            this.total += this.runningTotal;
+            this.runningTotal = 0;
+            // Uncheck checkboxes
+            angular.forEach(this.options, function (option) {
+                option.Selected = false;
+            });
+            this.$http.post('https://cors-anywhere.herokuapp.com/http://acucafe.acumen.rocks/api/Order', drinks)
+                .then(function () {
+                alert("Your order will be with you shortly!!");
+            }, function () {
+                alert("Could not post your order");
+            });
         };
-
-    }
-})();
+        ;
+        return drinkController;
+    }());
+    drinkController.$inject = ['$http'];
+    app.drinkController = drinkController;
+    angular
+        .module('acuCafe')
+        .controller('drinkController', [
+        '$http', drinkController
+    ]);
+})(app || (app = {}));
